5637ac41bfb5f087995e0abce3cdca097e102597
Quentin Barroché <barrocheq@gmail.com>
2017-03-12 21:55:00 +0100

    Copie main (PartieFoufou) to main (PlateauFoufou)

--- src/main/java/jeux/PlateauFouFou.java
+++ src/main/java/jeux/PlateauFouFou.java
@@ -308,14 +308,47 @@ public class PlateauFouFou implements Partie1 {
 
     public static void main(String[] args) {
 
-        PlateauFouFou p = new PlateauFouFou();
+        String jBlanc = "blanc";
+        String jNoir = "noir";
 
-        //p.saveToFile("test.txt");
-        p.setFromFile("test.txt");
-        p.printPlateau();
-//        p.mouvementsPossibles("blanc");
-        p.play("C6-B5", "blanc");
-        System.out.println("PRINT NEW TABLEAU");
-        p.printPlateau();
+        String[] lesJoueurs = new String[2];
+        lesJoueurs[0] = jBlanc;
+        lesJoueurs[1] = jNoir;
+
+//        AlgoJeu AlgoJoueur[] = new AlgoJeu[2];
+//        AlgoJoueur[0] = new Minimax(HeuristiquesDominos.hblanc, jBlanc, jNoir, 4); // Il faut remplir la méthode !!!
+//        AlgoJoueur[1] = new Minimax(HeuristiquesDominos.hnoir, jNoir, jBlanc, 1);  // Il faut remplir la méthode !!!
+
+
+        boolean jeufini = false;
+        int jnum;
+
+        PlateauFouFou plateauCourant = new PlateauFouFou();
+
+        // A chaque itération de la boucle, on fait jouer un des deux joueurs
+        // tour a tour
+        jnum = 0; // On commence par le joueur Blanc (arbitraire)
+
+        while (!jeufini) {
+            String[] meilleurCoup = plateauCourant.mouvementsPossibles(lesJoueurs[jnum]);
+
+            System.out.println("Coups possibles pour " + lesJoueurs[jnum] + " : ");
+            PartieFouFou.printCoupPossible(meilleurCoup);
+
+            if(meilleurCoup.length != 0 ) {
+                System.out.println("Coup joué : " + meilleurCoup[0] + " par le joueur " + lesJoueurs[jnum]);
+
+                plateauCourant.play(meilleurCoup[0], lesJoueurs[jnum]);
+                plateauCourant.printPlateau();
+
+
+                jnum = 1 - jnum;
+
+            } else {
+                System.out.println("Le joueur " + lesJoueurs[jnum] + " ne peut plus jouer et abandone !");
+                System.out.println("Le joueur " + lesJoueurs[1 - jnum] + " a gagné cette partie !");
+                jeufini = true;
+            }
+        }
     }
 }
