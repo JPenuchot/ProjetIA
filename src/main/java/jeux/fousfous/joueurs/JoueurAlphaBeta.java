ff519bbd336fb5f8ec91648ff17c8ed30c5d987f
Quentin Barroché <barrocheq@gmail.com>
2017-04-07 14:53:06 +0200

    3 if

--- src/main/java/jeux/fousfous/joueurs/JoueurAlphaBeta.java
+++ src/main/java/jeux/fousfous/joueurs/JoueurAlphaBeta.java
@@ -129,9 +129,13 @@ public class JoueurAlphaBeta implements IJoueur {
                     //  Trier les coups en fonction de leurs valeurs retournées
                     //  Mais on n'aura pas le temps donc bon... Tant pis I guess
 
-            mem = BaseAlphaBeta.find(this.plateau);
+            try {
+                mem = BaseAlphaBeta.find(this.plateau);
+            } catch (Exception e) {
+                System.out.println("Erruer find : " + e);
+            }
 
-            if(mem == null || mem.prof < p) {
+            if(mem == null) {
                 String[] coupPossibles = this.plateau.mouvementsPossibles(this.player);
 
                 // for(String c : coupPossibles) {
@@ -140,15 +144,18 @@ public class JoueurAlphaBeta implements IJoueur {
 
 
                 for(String c : coupPossibles) {
-
-                    System.out.println("c: " + c);
-
-                    Action[] ac = new Action[2];
+                    Action[] ac;
                     ac = this.plateau.play(c, this.player);
 
                     alpha = Math.max(alpha, - negAlphaBeta(p-1, - beta, - alpha, - parite));
 
-                    mem = BaseAlphaBeta.add(this.plateau);
+                    
+                    try {
+                        mem = BaseAlphaBeta.add(this.plateau);
+                    } catch(Exception e) {
+                        System.out.println("Erreur mem : " + e);
+                    }
+
                     mem.alpha = alpha;
                     mem.beta = beta;
                     mem.prof = p;
@@ -163,7 +170,37 @@ public class JoueurAlphaBeta implements IJoueur {
                         return beta;
                     }
                 }
-            } else {
+            } else if (mem.prof < p) {
+                String[] coupPossibles = this.plateau.mouvementsPossibles(this.player);
+
+                // for(String c : coupPossibles) {
+                //     Action[] ac = new Action[2];
+                // }
+
+
+                for(String c : coupPossibles) {
+                    Action[] ac;
+                    ac = this.plateau.play(c, this.player);
+
+                    alpha = Math.max(alpha, - negAlphaBeta(p-1, - beta, - alpha, - parite));
+
+
+                    mem.alpha = alpha;
+                    mem.beta = beta;
+                    mem.prof = p;
+
+
+                    for(Action a : ac) {
+                        a.reverse();
+                        this.plateau.play(a);
+                    }
+
+                    if(alpha >= beta) {
+                        return beta;
+                    }
+                }
+            }
+            else {
                 System.out.println("Mem deja trouvé : " + mem.alpha);
                 alpha = mem.alpha;
             }
